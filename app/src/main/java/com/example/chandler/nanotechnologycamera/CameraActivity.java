4fa33432569d15a0d84681ef83f709c193bbd1fb
ccm4n2 <ccm4n2@mail.missouri.edu>
2015-09-11 16:38:28 -0500

    Nanotechnology Camera App files

---
+++ app/src/main/java/com/example/chandler/nanotechnologycamera/CameraActivity.java
@@ -0,0 +1,34 @@
+package com.example.chandler.nanotechnologycamera;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        //creates intent for image capture by the other application
+        Intent intent = new Intent();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
